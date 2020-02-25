c530c58e8631c018f0c00c2d6b0664b28edf38d9
Soul1 <darkwaud@gmail.com>
2020-02-25 17:18:25 +0300

    update UsersPage, added paginate in UsersPage

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -10,16 +10,42 @@ class Users extends React.Component {
     }
 
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (pageNum) => {
+        this.props.setCurrentPage(pageNum)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNum}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
     }
 
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
         return (
 
             <div className={s.inner}>
+                <div className={s.paginate}>
+                    {pages.map(p => {
+                        return <span
+                            className={this.props.currentPage === p && s.selectedPage || s.selectPage}
+                            onClick={() => {
+                                this.onPageChanged(p)
+                            }}>{p}</span>
+                    })}
+                </div>
                 {
                     this.props.users.map(u => <div key={u.id} className={s.user}>
 
@@ -46,7 +72,7 @@ class Users extends React.Component {
                         <div className={s.wrapper}>
                             <div className={s.info}>
                                 <h3>{u.name}</h3>
-                                <p>{u.status}</p>
+                                <p>{u.status !== ''? u.status: "Нет статуса"}</p>
                             </div>
                             <div className={s.location}>
                                 <div>{"u.location.country"}</div>
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,11 +1,15 @@
 import React from "react";
 import {connect} from "react-redux";
 import Users from "./Users";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setUsersTotalCountAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
 
 const mapStateToProps = (state) => {
     return{
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
+
     }
 }
 
@@ -19,6 +23,12 @@ const mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (currentPage) => {
+            dispatch(setCurrentPageAC(currentPage))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setUsersTotalCountAC(totalCount))
         }
     }
 }
